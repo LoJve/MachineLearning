b2ba098fe589875abe21f8de2bd5612e7493ab6b
LoJve <Hjdong8@163.com>
2019-04-05 15:06:51 +0800

    face detection by face recognition

--- Instance/face_login/demo/face_detection/FaceRecognitionDemo.py
+++ Instance/face_login/demo/face_detection/FaceRecognitionDemo.py
@@ -11,11 +11,21 @@
 
 # Here put the import lib
 # https://www.cnblogs.com/jyxbk/p/7677877.html
+# https://www.jianshu.com/p/0b37452be63e
 import face_recognition
 import os
+import cv2 as cv
 
 image_path = os.path.abspath("./MachineLearning/Instance/face_login/image")
 image_name = "test.jpg"
 
 image = face_recognition.load_image_file(os.path.join(image_path, image_name))
-face_locations = face_recognition.face_locations(image)
+# model = 'cnn', 速度慢，但是精度稍微高一些
+# model = 'hog', 速度快，精度低
+face_locations = face_recognition.face_locations(image, model='cnn')
+
+image_cv = cv.imread(os.path.join(image_path, image_name))
+for top, right, bottom, left in face_locations:
+    cv.rectangle(image_cv, (left, top), (right, bottom), (0, 255, 0), 2)
+cv.imshow('image', image_cv)
+cv.waitKey()
